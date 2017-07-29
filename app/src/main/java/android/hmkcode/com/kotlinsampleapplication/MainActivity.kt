98cd754931fc0cc9fb9667eee14080e3a3c63a5b
LENOVO <lenovo@ubuntu.ubuntu-domain>
2017-07-29 22:58:50 +0530

    master branch commit

---
+++ app/src/main/java/android/hmkcode/com/kotlinsampleapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package android.hmkcode.com.kotlinsampleapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.test) as TextView
+        textView!!.text = "Hello World!"
+    }
+}
