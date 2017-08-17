5093a3b3df3db681d07f8cfb66fe53f68feb9eba
LENOVO <lenovo@ubuntu.ubuntu-domain>
2017-08-18 04:11:31 +0530

    dev2 commit

--- app/src/main/java/android/hmkcode/com/kotlinsampleapplication/MainActivity.kt
+++ app/src/main/java/android/hmkcode/com/kotlinsampleapplication/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.test) as TextView
         textView!!.text = "Welcome"
+//commit
     }
 }
